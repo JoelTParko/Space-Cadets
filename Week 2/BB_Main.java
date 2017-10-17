c3326d6c617d49fccf89ed35f3b78ba5d6722c99
Joel Parkinson <32489040+JoelTParko@users.noreply.github.com>
2017-10-17 17:32:17 +0100

    check

--- Week 2/BB_Main.java
+++ Week 2/BB_Main.java
@@ -1,28 +1,12 @@
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 
 public class BB_Main {
     public static void main(String[] args)throws Exception{
         String fileName;
-        String fileLocation;
         fileName = getInput();
-        
-        fileLocation = System.getenv("UserProfile") + "\\Documents\\BareBones\\" + fileName + ".txt";
-        System.out.println(fileLocation);
-        //Test if the file exists.
-		File testFile = new File(fileLocation);
-		if (testFile.exists()) {
-			System.out.println("File verified");
-		}
-		else {
-			System.out.println("File doesn't exist, exiting program.");
-			System.exit(0);
-		}
-		
-		FileReader bbCode = new FileReader(fileLocation);
-		
+        FileReader bbCode = new FileReader("C:\\Users\\Joelt\\Space Cadets Programs\\Barebones\\" + fileName + ".txt");
         bbCode.readFile();
 
     }
