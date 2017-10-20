1df4eb47ea893bab722851957a5f6a7b3b27dec9
Joel Parkinson <32489040+JoelTParko@users.noreply.github.com>
2017-10-20 22:47:00 +0100

    Re-worked Interpreter
    The interpreter class now does both the decoding, and executing of the BB code. The program works the same as before, but it is much easier to adapt it. Functional extensions have been commented out.

--- Week 3/Space_Cadets_W2/src/BB_Interpreter.java
+++ Week 3/Space_Cadets_W2/src/BB_Interpreter.java
@@ -1,34 +1,117 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.*;
+import java.util.*;
 
 public class BB_Interpreter {
-	
-    public String[] commands = {"clear", "incr", "decr", "while", "end"};
-    public Map<String, Integer> variables = new HashMap<>();
+    private boolean inWhile;
+    private int whileCount = 0;
+    private int startIndex = 0;
+    private int endIndex;
+    private Map<String, Integer> functions = new HashMap<>();
+    private Stack<Integer> whileStack = new Stack<>();
+    private String[] commands = {"clear", "incr", "decr", "while", "end", ""};
+    private Map<String, Integer> variables = new HashMap<>();
+    private List<String> fileLines;
 
-    public String[] readLine(String currentLine){
-        String varName = null;
-        String[] instruction;
+    public BB_Interpreter(List<String> fileLines){
+        this.fileLines = fileLines;
+        this.endIndex = fileLines.size();
 
-        //Loop through all of the commands
-        for (String patternString: commands) {
-            Pattern pattern = Pattern.compile("^(?:\\s*"+patternString+"\\s+(\\w+)(?:\\s+not\\s+\\d+\\s+do)?\\s*;\\s*)|(?:\\s*"+patternString+"\\s*;\\s*)$");
-            Matcher matcher = pattern.matcher(currentLine);
-            if(matcher.find()){
-                if (patternString != "end") {
-                    varName = matcher.group(1);
-                }
-                //Create and return the instruction.
-                instruction = new String[]{patternString, varName};
-                return instruction;
-            }
-        }
-
-        //Returns null if no instruction is found.
-        return  null;
+        //findFunctions();
     }
 
+    public BB_Interpreter(List<String> fileLines, int startIndex, int endIndex){
+        this.fileLines = fileLines;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+
+        //findFunctions();
+    }
+
+    /*
+    public void findFunctions(){
+        String currentLine;
+        String functionName;
+        boolean foundFunction;
+        int index, index2;
+        Pattern pattern = Pattern.compile("^func\\s+(\\w+)\\s*:\\s*$");
+        Iterator<String> iterator = fileLines.listIterator(startIndex);
+        index = startIndex;
+
+        while(index < endIndex){
+            index++;
+            currentLine = iterator.next();
+            Matcher match = pattern.matcher(currentLine);
+            if(match.find()){
+                functionName = match.group(1);
+            }
+        }
+    }
+*/
+    public int next(String currentLine, int index){
+
+        String token;
+        int jumpPoint = 0;
+
+        //Removes comments from the code
+        currentLine = deCommenter(currentLine);
+        /*
+        if(checkForFunction(currentLine)){
+
+        }
+        */
+
+
+        token = readToken(currentLine); //Identifies the token, will execute the code if its a basic command/Checks for condition met in while loops
+        if(token == "while"){
+            if(!inWhile){ //Jumps to the end of a while loop if the condition was met
+                jumpPoint = endIndex;
+            }else{ //Adds the location of the current while statement to the while stack
+                whileStack.push(index - 1);
+                whileCount++;
+            }
+        }else if(token == "end" && whileCount > 0){//Checks if the program should jump back to the previous while statement or not
+            jumpPoint = whileStack.pop();
+            endIndex = index;
+            whileCount--;
+        }
+
+        return jumpPoint;
+    }
+/*
+    public boolean checkForFunction(String currentLine){ //checks for a function call
+        Pattern pattern = Pattern.compile("^func\\s+(\\w+)\\(\\);\\s*$");
+        Matcher match = pattern.matcher(currentLine);
+        if(match.find()){
+            return true;
+        }
+        return false;
+    }
+    */
+
+    public String readToken(String currentLine) {
+        String varName;
+
+        //Loop through all of the commands
+        for (String token : commands) {
+            Pattern pattern = Pattern.compile("^(?:\\s*" + token + "\\s+(\\w+)(?:\\s+not\\s+(\\d+)\\s+do)?\\s*;\\s*)|(?:\\s*" + token + "\\s*;\\s*)$");
+            Matcher matcher = pattern.matcher(currentLine);
+            if (matcher.find()) { //Checks if the current token matches the one in the BB code
+                if (token != "end") {
+                    varName = matcher.group(1); //Finds the name of the variable that is being used
+                    if (token == "while") {
+                        inWhile = whileCheck(varName, matcher.group(2)); //Checks if the while condition has been met
+                    } else {
+                        executeCommand(token, varName); //Executes one of the three basic commands
+                    }
+                }
+                return token;
+            }
+        }
+        return null;
+    }
+
+    
     
     public void executeCommand(String command, String varName){
     	//Execute the command, based on the operator and operand.
@@ -42,18 +125,16 @@ public class BB_Interpreter {
             case "decr":
                 variables.put(varName, variables.get(varName)-1);
                 break;
+
             default:
         }
     }
     
-    public boolean executeWhile(String varName, String line){
-        Pattern pattern = Pattern.compile("not");
-        Matcher matcher = pattern.matcher(line);
-        
-        if(matcher.find()){
-            if (variables.get(varName) == Character.getNumericValue(line.charAt(matcher.end() + 1))) return  false;
+    public boolean whileCheck(String varName, String value){
+
+        if (variables.get(varName) == Integer.parseInt(value)) {
+            return false;
         }
-        
         return true;
     }
     
